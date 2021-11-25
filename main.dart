9e396dce4135e5ec4dfb8bffdf9391711c40976f
Kevinfame007 <kevinfame007@gmail.com>
2021-11-25 16:43:44 +0700

    learn how to use static variable type
    learn how to use static variable type

--- main.dart
+++ main.dart
@@ -2,6 +2,14 @@ void main () {
   /*
     comment  
   */
+  int age = 20;
+  double pi = 3.14;
+  String name = "fame";
+  bool isCheck = true;
+  print(age);
   print("Hello Dart");
+  print(pi);
+  print(name);
+  print(isCheck);
+  
 }
-
