e68f1e4bbd70497f2a4e4f15770884252cabf91f
Kevinfame007 <kevinfame007@gmail.com>
2021-11-25 19:08:47 +0700

    learn how to use dynamic type
    learn how to use dynamic type

--- main.dart
+++ main.dart
@@ -2,6 +2,27 @@ void main () {
   /*
     comment  
   */
-  print("Hello Dart");
-}
 
+  //Static type
+  // int age = 20;
+  // double pi = 3.14;
+  // String name = "fame";
+  // bool isCheck = true;
+
+  //dynamic type
+  var age = 20;
+  var pi = 3.14;
+  var name = "fame";
+  var isCheck = true;
+
+  dynamic name2 = "kun";
+
+  print(age);
+  print("Hello Dart");
+  print(pi);
+  print(name);
+  print(isCheck);
+
+  print(name2);
+  
+}
