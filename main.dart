b21508719e98e1a5601234fab09b085145a27c28
Kevinfame007 <kevinfame007@gmail.com>
2021-11-25 19:10:08 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,5 @@
 void main () {
-  /*
-    comment  
-  */
-
-  //Static type
-  // int age = 20;
-  // double pi = 3.14;
-  // String name = "fame";
-  // bool isCheck = true;
-
-  //dynamic type
+ 
   var age = 20;
   var pi = 3.14;
   var name = "fame";
