5c000c9af0b9847cf2e15c604386765818b69fdd
Kevinfame007 <kevinfame007@gmail.com>
2021-11-25 21:14:54 +0700

    learn how to use operator in dart
    learn how to use operator and operand in dart language

--- main.dart
+++ main.dart
@@ -1,16 +1,7 @@
 void main () {
 
-  // var fname = 'Suphanut';
-  // String lname = 'Taengtoemphon';
-
-  // print(fname);
-  // print(lname);
-
-  var name = "fame";
-  var city = "nonthaburi";
-  var age = 20;
-  var number1 = 50;
-
-  print("ชื่อ = $name อายุ = ${age+number1}");
+  int x = 100, y = 30;
+  int z = (x % y).toInt();
+  print(z);
 
 }
