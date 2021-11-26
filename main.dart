f6271b527a242068754048ecef44da8c982d8133
Kevinfame007 <kevinfame007@gmail.com>
2021-11-26 10:58:40 +0700

    learn how to use loop while
    learn how to use loop while

--- main.dart
+++ main.dart
@@ -1,20 +1,9 @@
 void main() {
-  var month = "one";
-  String name;
-
-  switch (month) {
-    case "one":
-      name = "january";
-      break;
-    case "two":
-      name = "febuary";
-      break;
-    case "three":
-      name = "march";
-      break;
-    default:
-      name = "not found data";
+  int count = 1;
+  int stop = 10;
+  while (count<=stop) {
+    print("ผลิตรอบที่ = $count");
+    count++;
   }
-
-  print("month $month = $name");
+  print("จบโปรแกรม");
 }
