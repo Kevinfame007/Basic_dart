e78044fca46b25488e2d487edd8846e24c7d606b
Kevinfame007 <kevinfame007@gmail.com>
2021-11-26 12:54:57 +0700

    learn how to use break and continue
    learn how to use break and continue

--- main.dart
+++ main.dart
@@ -1,9 +1,11 @@
 void main() {
   
-  var count = 10;
-  do {
-    print("Hello Dart = $count");
-    count++;
-  } while (count<=3);
+  var number = 2;
+  for (var count = 1; count <= 12; count++) {
+    if (count % 2 == 0) {
+      continue;
+    }
+    print("$number x $count = ${number * count}");
+  }
   print("End");
 }
